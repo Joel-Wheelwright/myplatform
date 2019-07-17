1a85c02675c55f0c92c589e5b809342f1769b800
Joel-Wheelwright <52959338+Joel-Wheelwright@users.noreply.github.com>
2019-07-17 13:19:41 +0200

    Add files via upload

--- Controllers/DeviceController.js
+++ Controllers/DeviceController.js
@@ -20,5 +20,22 @@ module.exports.createDevice = function(req, res){
         } else {
             res.status(400).send('Device creation failed');
         };
-    })
+    });
+};
+
+module.exports.getDevices = function(req, res){
+    DeviceModel.find({}).then(function(devices){
+        res.json(devices);
+    });
+};
+
+module.exports.getDeviceById = function(req, res){
+    const deviceId = req.query.id
+    DeviceModel.findById(deviceId).then(function(device) {
+        if (device){
+            res.json(device);
+        } else {
+            res.status (404).send('No device found with this id');
+        }
+    });
 };
